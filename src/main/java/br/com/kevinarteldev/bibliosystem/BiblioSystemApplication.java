a24e6f365d64e6d23361cf6aa69669941a9780f9
Kevin <kevinaryel.dev@gmail.com>
2023-01-10 15:03:55 -0300

    feat: start do projeto

---
+++ src/main/java/br/com/kevinarteldev/bibliosystem/BiblioSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.kevinarteldev.bibliosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kevinarteldev/bibliosystem/BiblioSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kevinarteldev.bibliosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
