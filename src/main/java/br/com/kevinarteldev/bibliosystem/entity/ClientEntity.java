798341c248e717cb6011ea205ea7948c19d93318
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-12 17:03:53 -0300

    refactor: ajuste na tipagem do id

--- src/main/java/br/com/kevinarteldev/bibliosystem/entity/ClientEntity.java
+++ src/main/java/br/com/kevinarteldev/bibliosystem/entity/ClientEntity.java
@@ -2,24 +2,26 @@ package br.com.kevinarteldev.bibliosystem.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
-import java.util.UUID;
-
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "Client")
+@Table(name = "Client")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class ClientEntity {
 
-    private static final long serialVersionUID = 1L;
-
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CLIENT_SEQ")
+    @SequenceGenerator(name = "CLIENT_SEQ", sequenceName = "SEQ_CLIENT", allocationSize = 1)
     @Column(name = "id")
-    private UUID id;
+    private Integer id;
     @Column(nullable = false)
     private String name;
     @Column(nullable = false)
@@ -30,5 +32,4 @@ public class ClientEntity {
     private Character gender;
     @Column(nullable = false)
     private Date birthDate;
-
 }
