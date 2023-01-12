14efa89d3fd230fa0e582a4bef4469f60ab29e73
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-12 17:08:27 -0300

    feat: remoção de query não utilizada

--- src/main/java/br/com/kevinarteldev/bibliosystem/repository/ClientRepository.java
+++ src/main/java/br/com/kevinarteldev/bibliosystem/repository/ClientRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<ClientEntity, Integer> {
-    @Query("Select * FROM Client ORDER BY id")
-    Page<ClientEntity> returnAll(Pageable pageable);
 }
