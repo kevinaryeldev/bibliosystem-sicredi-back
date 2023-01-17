de57fc7f0cd9e380b08d5457e81d73f2aa523398
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-17 15:55:21 -0300

    feat: Add funcao de editar

--- src/main/java/br/com/kevinarteldev/bibliosystem/service/ClientService.java
+++ src/main/java/br/com/kevinarteldev/bibliosystem/service/ClientService.java
@@ -1,11 +1,13 @@
 package br.com.kevinarteldev.bibliosystem.service;
 
-import br.com.kevinarteldev.bibliosystem.dto.ClientCreateDTO;
-import br.com.kevinarteldev.bibliosystem.dto.ClientDTO;
-import br.com.kevinarteldev.bibliosystem.dto.PageDTO;
+import br.com.kevinarteldev.bibliosystem.request.ClientCreateRequest;
+import br.com.kevinarteldev.bibliosystem.request.ClientEditRequest;
+import br.com.kevinarteldev.bibliosystem.response.ClientResponse;
+import br.com.kevinarteldev.bibliosystem.response.PageResponse;
 import br.com.kevinarteldev.bibliosystem.entity.ClientEntity;
 import br.com.kevinarteldev.bibliosystem.repository.ClientRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,42 +15,51 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ClientService {
     private final ObjectMapper objectMapper;
     private final ClientRepository clientRepository;
-    public void create(ClientCreateDTO client){
+    @Transactional
+    public ClientResponse create(ClientCreateRequest client){
         ClientEntity clientEntity = objectMapper.convertValue(client, ClientEntity.class);
         ClientEntity clientSaved = clientRepository.save(clientEntity);
-        objectMapper.convertValue(clientSaved, ClientDTO.class);
-        return;
+        return objectMapper.convertValue(clientSaved, ClientResponse.class);
     }
-//    public ClientDTO edit(Integer id, ClientCreateDTO client){
-//        ClientEntity clientEntity = objectMapper.convertValue(client, ClientEntity.class);
-//        clientRepository.
-//    }
-    public PageDTO<ClientDTO> list(Integer page, Integer size){
+    @Transactional
+    public ClientResponse edit(Integer id, ClientEditRequest client){
+        Optional<ClientEntity> clientOpt = clientRepository.findById(id);
+        if(clientOpt.isPresent()){
+            ClientEntity clientSaved = clientOpt.get();
+            clientSaved.setEmail(client.getEmail());
+            clientSaved.setGender(client.getGender());
+            return objectMapper.convertValue(clientRepository.save(clientSaved), ClientResponse.class);
+        }
+        return null;
+    }
+    public PageResponse<ClientResponse> list(Integer page, Integer size){
         Sort orderBy = Sort.by("id");
         PageRequest pageRequest = PageRequest.of(page,size,orderBy);
         Page<ClientEntity> repositoryPage = clientRepository.findAll(pageRequest);
-        List<ClientDTO> clientList = repositoryPage
+        List<ClientResponse> clientList = repositoryPage
                 .getContent()
                 .stream()
-                .map(client -> objectMapper.convertValue(client,ClientDTO.class))
+                .map(client -> objectMapper.convertValue(client, ClientResponse.class))
                 .toList();
-        return new PageDTO<>(repositoryPage.getTotalElements(),
+        return new PageResponse<>(repositoryPage.getTotalElements(),
                 repositoryPage.getTotalPages(),
                 page,
                 size,
                 clientList);
     }
+    @Transactional
     public void delete(Integer id){
         clientRepository.deleteById(id);
     }
-    public ClientDTO findById(Integer id){
+    public ClientResponse findById(Integer id){
         ClientEntity client = clientRepository.findById(id).orElseThrow(()-> new RuntimeException("Client not found"));
-        return objectMapper.convertValue(client, ClientDTO.class);
+        return objectMapper.convertValue(client, ClientResponse.class);
     }
 }
