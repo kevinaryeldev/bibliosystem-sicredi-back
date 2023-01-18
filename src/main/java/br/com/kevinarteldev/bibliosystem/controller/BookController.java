1c797fc14d1cef45f15b2c5f05b01666f082ea56
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-18 19:25:37 -0300

    feat: iniciando a criação dos endpoints

--- src/main/java/br/com/kevinarteldev/bibliosystem/controller/BookController.java
+++ src/main/java/br/com/kevinarteldev/bibliosystem/controller/BookController.java
@@ -1,22 +1,36 @@
 package br.com.kevinarteldev.bibliosystem.controller;
 
+import br.com.kevinarteldev.bibliosystem.request.BookCreateRequest;
+import br.com.kevinarteldev.bibliosystem.response.BookResponse;
+import br.com.kevinarteldev.bibliosystem.response.PageResponse;
 import br.com.kevinarteldev.bibliosystem.service.BookService;
-import lombok.RequiredArgsConstructor;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Validated
 @Slf4j
 @RestController
 @RequestMapping("/book")
 public class BookController {
-
     private final BookService bookService;
-
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
-
+    @PostMapping
+    public ResponseEntity<BookResponse> create(@Valid @RequestBody BookCreateRequest book){
+        return new ResponseEntity<>(bookService.create(book),HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> list(Integer page, Integer size){
+        return new ResponseEntity<>(bookService.list(page,size), HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        bookService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
