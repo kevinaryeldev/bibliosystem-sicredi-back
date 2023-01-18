04978536c964f9e0f255379944b1243bed7ad8ab
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-18 19:26:54 -0300

    feat: exception para erros de negócio

---
+++ src/main/java/br/com/kevinarteldev/bibliosystem/exception/BusinessRuleException.java
@@ -0,0 +1,9 @@
+package br.com.kevinarteldev.bibliosystem.exception;
+
+public class BusinessRuleException extends Exception {
+
+        public BusinessRuleException(String message) {
+            super(message);
+        }
+
+}
