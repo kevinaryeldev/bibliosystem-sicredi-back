d0323c746f9fd8afb3b65c197603ebe46a5517f1
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-18 19:27:56 -0300

    feat: exception para erros tipo 404

---
+++ src/main/java/br/com/kevinarteldev/bibliosystem/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.kevinarteldev.bibliosystem.exception;
+
+public class NotFoundException extends Exception {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
