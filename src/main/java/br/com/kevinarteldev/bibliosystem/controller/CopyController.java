23c83c8f434c413cffff878ba188795f2971daac
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-20 03:00:44 -0300

    refactor: removido import não utilizado

--- src/main/java/br/com/kevinarteldev/bibliosystem/controller/CopyController.java
+++ src/main/java/br/com/kevinarteldev/bibliosystem/controller/CopyController.java
@@ -2,7 +2,6 @@ package br.com.kevinarteldev.bibliosystem.controller;
 
 
 import br.com.kevinarteldev.bibliosystem.service.CopyService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
