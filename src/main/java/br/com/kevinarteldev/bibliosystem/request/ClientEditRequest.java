da07529097addc020455e3829a7023a4f41525fa
KevinAryel <kevinaryel.dev@gmail.com>
2023-01-20 00:04:30 -0300

    refactor: remoção de validações

--- src/main/java/br/com/kevinarteldev/bibliosystem/request/ClientEditRequest.java
+++ src/main/java/br/com/kevinarteldev/bibliosystem/request/ClientEditRequest.java
@@ -1,16 +1,9 @@
 package br.com.kevinarteldev.bibliosystem.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 @Data
 public class ClientEditRequest {
-
-    @NotBlank
-    @Email
     private String email;
-
-    @NotNull
+    private String name;
     private Character gender;
 }
